5a2d25744eb0c60b87b8623027edbefd9893f812
saipavan <saipavansiripuram5@gmail.com>
2022-05-17 11:59:19 +0530

    First commit todo

---
+++ src/components/TodoList.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+
+function TodoList() {
+    let initTodo;
+    if (localStorage.getItem("todos") === null) {
+      initTodo = [];
+    }
+    else {
+      initTodo = JSON.parse(localStorage.getItem("todos"));
+    }
+  
+    const [todos, setTodos] = useState(initTodo);
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos])
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+    localStorage.setItem("todos", JSON.stringify(todos))
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>What's the Plan for Today?
+      <br></br><span className="xyz">
+      {`${new Date().toLocaleString()}`}
+</span>
+      </h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+export default TodoList;
